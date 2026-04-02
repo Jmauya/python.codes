e41db0f37c784f7d1cf969274e384249def3a1f9
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-02 21:42:32 +0600

    Variable Assignment.py
    In Python, a variable allows you to refer to a value with a name. To create a variable x with a value of 5, you use =, like this example.

---
+++ Exercises/Variable Assignment.py
@@ -0,0 +1,9 @@
+//Instructions
+//Create a variable savings with the value of 100.
+//Check out this variable by typing print(savings) in the script.
+  
+# Create the variable
+savings = 100
+
+# Print the variable
+print(savings)
