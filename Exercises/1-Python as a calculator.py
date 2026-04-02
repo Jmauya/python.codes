547351472216ab22e597864fbcd5056b8e553597
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-02 21:17:43 +0600

    1-Python as a calculator.py

---
+++ Exercises/1-Python as a calculator.py
@@ -0,0 +1,20 @@
+// Instructions
+
+//Print the result of subtracting 5 from 5 under # Subtraction using print().
+// Print the result of multiplying 3 by 5 under # Multiplication.
+//Print the result of subtracting 5 from 5 under # Subtraction using print().
+//Print the result of multiplying 3 by 5 under # Multiplication.
+
+# Subtraction
+print(5 - 5)
+
+# Multiplication
+print(3 * 5)
+
+# Subtraction
+print(5 - 5)
+
+# Multiplication
+print(3 * 5)
+
+
