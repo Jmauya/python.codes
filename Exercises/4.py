8a71648351b281fa4b562f6b50fd43e1fc538712
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-03 23:59:05 +0600

    4.py

--- Exercises/4.py
+++ Exercises/4.py
@@ -1,3 +1,17 @@
+# Other variable types
+# In the previous exercise, you worked with the integer Python data type:
+# int, or integer: a number without a fractional part.
+
+# Next to numerical data types, there are three other very common data types:
+# float: a number with a decimal point
+# str: text (string)
+# bool: logical values (True or False)
+
+# Instructions:
+# Create a new float, half, with the value 0.5.
+# Create a new string, intro, with the value "Hello! How are you?".
+# Create a new boolean, is_good, with the value True.
+
 # Float
 half = 0.5
 
@@ -7,7 +21,7 @@ intro = "Hello! How are you?"
 # Boolean
 is_good = True
 
-# Print them (optional)
+# Print them
 print(half)
 print(intro)
 print(is_good)
