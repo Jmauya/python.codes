731f8b3bb3023e283f2acfb6e0f9d7494c48067f
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-04 00:01:34 +0600

    5.py

---
+++ Exercises/5.py
@@ -0,0 +1,17 @@
+# Operations with other types
+# Variables come in different types in Python.
+# You can see the type of a variable by using type().
+
+# Instructions:
+# 1. Add savings and new_savings and assign it to total_savings.
+# 2. Use type() to print the resulting type of total_savings.
+
+# Given variables
+savings = 100
+new_savings = 40
+
+# Add them
+total_savings = savings + new_savings
+
+# Print the type
+print(type(total_savings))
