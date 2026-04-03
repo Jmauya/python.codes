95fc771f8fd3adba641aaed70988b7b63dfa5b4c
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-03 23:56:11 +0600

    3.py

---
+++ Exercises/3.py
@@ -0,0 +1,21 @@
+//Calculations with variables
+//You've now created a savings variable, so let's start saving!
+//Instead of calculating with the actual values, you can use variables instead.
+
+//ow much money would you have saved four months from now, if you saved $10 each month?
+
+//Instructions
+
+//Create a variable monthly_savings, equal to 10 and num_months, equal to 4.
+//Multiply monthly_savings by num_months and assign it to new_savings.
+//Print the value of new_savings.
+
+# Create variables
+monthly_savings = 10
+num_months = 4
+
+# Calculate total savings
+new_savings = monthly_savings * num_months
+
+# Print the result
+print(new_savings)
