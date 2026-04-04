aa7228cbb5dbb5959a2a9b43f4463de436a76c4d
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-05 00:16:15 +0600

    7.py

---
+++ Exercises/7.py
@@ -0,0 +1,16 @@
+# Create lists with different types
+# Instructions
+# Finish the code that creates the areas list. Build the list so that the list- 
+# first contains the name of each room as a string and then its area.
+# In other words, add the strings "hallway", "kitchen" and "bedroom" 
+# at the appropriate locations.
+
+# create the mixed list
+areas = ["hallway", hall,
+         "kitchen", kit,
+         "living room", liv,
+         "bedroom", bed,
+         "bathroom", bath]
+
+# print the list
+print(areas)
