c3bdf3d6d270e3c99e8dc782ab44db10a1d28bad
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-05 00:12:29 +0600

    6.py

---
+++ Exercises/6.py
@@ -0,0 +1,22 @@
+# Measure the height of your family, you decide to collect some information 
+# on the house you're living in. The areas of the different parts of your house 
+# are stored in separate variables in the exercise.
+
+# Instructions
+# Create a list, areas, that contains the area of the hallway (hall), kitchen (kit), 
+# living room (liv), bedroom (bed) and bathroom (bath), in this order. 
+# Use the predefined variables.
+# Print areas with the print() function.
+
+# predefined variables (example values)
+//hall = 11.25
+//kit = 18.0
+//liv = 20.0
+//bed = 10.75
+//bath = 9.50
+
+# create the list
+areas = [hall, kit, liv, bed, bath]
+
+# print the list
+print(areas) 
