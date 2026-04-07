156bc7e2ba78e662885804407b1c7ffc7ad222b8
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-07 23:18:11 +0600

    8.Numpy.py

--- Exercises/8.Numpy.py
+++ Exercises/8.Numpy.py
@@ -1,7 +1,8 @@
 # Import the numpy package as np
 import numpy as np
+
 # Create a list of baseball player heights (example data)
-baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+baseball = [180, 215, 210, 210, 188, 176,209, 200]
 
 # Use np.array() to create a numpy array from baseball: np_baseball
 np_baseball = np.array(baseball)
