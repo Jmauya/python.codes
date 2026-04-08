7dedb60ca75887171352775417346113f39abac5
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-04-08 21:43:21 +0600

    9.Line plot.py

---
+++ Exercises/9.Line plot.py
@@ -0,0 +1,12 @@
+# print() the last item from both the year and the pop list
+print(year[-1])
+print(pop[-1])
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Use plt.plot() to build a line plot
+plt.plot(year, pop)
+
+# Display the plot
+plt.show()
