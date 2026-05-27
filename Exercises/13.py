0327bc1dfb6c8f3840156a6893a29da3e13b381c
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-05-27 22:17:30 +0600

    13.py

---
+++ Exercises/13.py
@@ -0,0 +1,11 @@
+# temperature.py
+
+# Current temperature of Brooklyn, NY in Fahrenheit
+fahrenheit = 77
+
+# Formula: Celsius = (Fahrenheit - 32) / 1.8
+celsius = (fahrenheit - 32) / 1.8
+
+# Printing the results
+print(f"Current Brooklyn temperature: {fahrenheit}°F")
+print(f"Converted to Celsius: {celsius:.1f}°C ")
