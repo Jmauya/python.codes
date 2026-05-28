c3dee89033ccdef9cbc02013632f64876c3194e6
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-05-28 23:06:35 +0600

    15.py

---
+++ Exercises/15.py
@@ -0,0 +1,8 @@
+import math
+
+a = float(input("Enter the length of side a: "))
+b = float(input("Enter the length of side b: "))
+
+c = math.sqrt((a ** 2) + (b ** 2))
+
+print(f"The length of the hypotenuse c is: {c:.2f}")
