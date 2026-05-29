a8a9127271b62f6009aa09229fb06b88c3b36ea7
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-05-29 21:20:47 +0600

    16.py

---
+++ Exercises/16.py
@@ -0,0 +1,19 @@
+# Even numbers (2, 4, 6, 8) and odd numbers (1, 3, 5, 7) pop up a lot in programming. 
+# Knowing them is key to solving many coding problems.
+# An even number is divisible by 2. An odd number leaves a remainder of 1 when divided by 2.
+# So how can we program this? We can use the % modulo operator to find the remainder:
+# Even → n % 2 == 0
+# Odd → n % 2 == 1
+# Create a num variable and give it any number.
+# Use a print() and % modulo operator to see if it's divisible by 2.
+
+# Create a num variable and give it any number
+num = 7
+
+# Use a print() and % modulo operator to see if it's divisible by 2
+print(num % 2)
+
+num = 8
+
+# This asks Python: "Is the remainder of num divided by 2 equal to 0?"
+print(num % 2 == 0)  # This will print: True
