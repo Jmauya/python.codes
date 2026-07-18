508199b2087727fc14f5e7e1f3e7da04442f1d18
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-07-18 23:23:28 +0600

    17.py

--- Exercises/17.py
+++ Exercises/17.py
@@ -5,6 +5,7 @@ scores = [45, 82, 94, 61, 55, 70, 88]
 passing_scores = []
 
 # Loop through each score to filter out anything below 60
+
 for score in scores:
     if score >= 60:
         passing_scores.append(score)
